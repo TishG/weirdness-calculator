ce1efc2af29e104fae0c49bf77b64a2dd4050b16
Tish <latisha.griffiths@programmer.net>
2019-11-19 20:50:47 -0600

    Add Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Weirdness Calculator</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
